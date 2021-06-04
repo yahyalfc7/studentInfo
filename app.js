ea2369661ddd8c6e0b5df4c4dbb3aee5e60b3ce6
yahyalfc <yahyarehmanlfc@gmail.com>
2021-06-04 16:41:53 +0500

    oauth with intuit

--- app.js
+++ app.js
@@ -1,74 +1,100 @@
 require("dotenv/config")
 const express = require("express")
-const mongoose = require("mongoose")
 const app = express()
-// Routes
-const authRoute = require('./routes/auth')
-const studentRoute = require('./routes/student')
-//Middlewares
-const verifyToken = require('./middlewares/verifyToken')
-const User = require("./models/User")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // convert into jsonn when responce comes
 
-//Specify the Amazon DocumentDB cert
-//var ca = [fs.readFileSync("rds-combined-ca-bundle.pem")];
+const OAuthClient = require('intuit-oauth');
+const QuickBooks = require('node-quickbooks')
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-mongoose.connect("mongodb://ec2-18-234-107-209.compute-1.amazonaws.com:27017/test", { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-  if (err) {
-    console.log(err)
-    throw err
-  }
-  console.log('connected');
-  // console.log('connected', db);
+const oauthClient = new OAuthClient({
+  clientId: 'ABk31jpYYZuG92bK3rrJ33jMKy3ZikI6wEsKjNfMJksjTPf6j1',
+  clientSecret: 'Ss5dT6RXMv6ri8hlUJOiKaIOyZtNyc67ahGFme0R',
+  environment: 'sandbox' || 'production',
+  redirectUri: 'http://localhost:9393/oauth2redirect',
 })
 
-app.get("/user", (req, res) => {
-  res.send('helloss')
-})
-
-app.get("/users", async (req, res) => {
+app.get('/auth', (req, res) => {
   try {
-    console.log(User);
-    const users = await User.find()
-    console.log('USERS', users);
-    res.status(200).json(users)
+    const authUri = oauthClient.authorizeUri({
+      scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
+      state: 'testState',
+    }); // can be an array of multiple scopes ex : {scope:[OAuthClient.scopes.Accounting,OAuthClient.scopes.OpenId]}
+    // Redirect the authUri which we have also saved in our sand box portal
+    res.redirect(authUri);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err)
+  }
+})
+
+app.get('/oauth2redirect', async (req, res) => {
+  const parseRedirect = req.url;
+  try {
+    const authResponce = await oauthClient.createToken(parseRedirect)
+    // const tokenString = JSON.stringify(authResponce.getJson());
+    //res.status(200).json(tokenString);
+    res.redirect('/getInfo')
+  } catch (err) {
+    console.log((err));
+    res.status(400).json(err)
+  }
+})
+
+app.get('/getInfo', async (req, res) => {
+  try {
+    const companyID = oauthClient.getToken().realmId;
+    const url = oauthClient.environment == 'sandbox'
+      ? OAuthClient.environment.sandbox
+      : OAuthClient.environment.production;
+
+    // console.log(oauthClient.token);
+    // console.log(oauthClient.token.access_token);
+
+    //This is wroking. We're using intuit oauth api for this. 
+    const authResponce = await oauthClient.makeApiCall({
+      //method: 'POST',
+      // headers: {
+      //   'Content-Type': 'application/json',
+      // },
+      //body: JSON.stringify(body), 
+      url: `${url}v3/company/${companyID}/companyinfo/${companyID}`
+    })
+
+    // This is using the quickbooks api
+    var qbo = new QuickBooks(
+      oauthClient.clientId,
+      oauthClient.clientSecret,
+      oauthClient.token.access_token,
+      false, // no token secret for oAuth 2.0
+      oauthClient.token.realmId,
+      true, // use the sandbox?
+      true, // enable debugging?
+      null, // set minorversion, or null for the latest version
+      '2.0', //oAuth version
+      oauthClient.token.refresh_token
+    );
+
+    // Error occurs here
+    qbo.getAccount()
+
+    res.status(200).json(authResponce.json)
   } catch (err) {
     console.log(err);
     res.status(400).json(err)
   }
 });
 
-app.post('/add-user', async (req, res) => {
-  const { name, rollno } = req.body;
-
-  try {
-    const user = User({
-      name, rollno
-    })
-    const result = await user.save()
-    console.log(result);
-    res.status(200).json(result)
-
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err)
-  }
-})
-
 app.get('*', (req, res) => {
   res.status(404).send('Page not found!')
 })
 
-
-
-// running the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9393;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
