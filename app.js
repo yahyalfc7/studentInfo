c83513c19eeff0b9c3fd92fd9f1298bd1a3730c7
yahyalfc <yahyarehmanlfc@gmail.com>
2021-06-10 13:20:57 +0500

    through way

--- app.js
+++ app.js
@@ -2,130 +2,46 @@ require("dotenv/config")
 const express = require("express")
 const app = express()
 
+const plaid = require('plaid')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // convert into jsonn when responce comes
 
-const OAuthClient = require('intuit-oauth');
-const QuickBooks = require('node-quickbooks')
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-const oauthClient = new OAuthClient({
-  clientId: 'ABk31jpYYZuG92bK3rrJ33jMKy3ZikI6wEsKjNfMJksjTPf6j1',
-  clientSecret: 'Ss5dT6RXMv6ri8hlUJOiKaIOyZtNyc67ahGFme0R',
-  environment: 'sandbox' || 'production',
-  redirectUri: 'http://localhost:9393/oauth2redirect',
-})
 
 app.get('/auth', (req, res) => {
   try {
-    const authUri = oauthClient.authorizeUri({
-      scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
-      state: 'testState',
-    }); // can be an array of multiple scopes ex : {scope:[OAuthClient.scopes.Accounting,OAuthClient.scopes.OpenId]}
-    // Redirect the authUri which we have also saved in our sand box portal
-    res.redirect(authUri);
+    const plaidClient = new plaid.Client({
+      clientID: '60c1b22d232f32000f863e0b',
+      secret: '67facb496511d425015c103855dcf3',
+      env: plaid.environments.sandbox,
+      options: {
+        version: '2020-09-14', // '2020-09-14' | '2019-05-29' | '2018-05-22' | '2017-03-08'
+      },
+    });
+    const publicToken = plaidClient.
+      res.send(publicToken)
   } catch (err) {
     console.log(err);
     res.status(400).json(err)
   }
 })
 
-app.get('/oauth2redirect', async (req, res) => {
-  const parseRedirect = req.url;
+app.get('/oauth2redirect', (req, res) => {
+  console.log('In redirect',);
+  console.log('request', req);
+  console.log("responce", res);
   try {
-    const authResponce = await oauthClient.createToken(parseRedirect)
-    const token = await oauthClient.getToken()
-    const tokenString = JSON.stringify(authResponce.getJson());
-    res.redirect('/getInfo')
+    res.status(200).json('Redirecting')
   } catch (err) {
-    console.error((err));
+    console.log(err)
     res.status(400).json(err)
   }
 })
-
-app.get('/getInfo', async (req, res) => {
-
-  try {
-    const companyID = oauthClient.getToken().realmId;
-    const url = oauthClient.environment == 'sandbox'
-      ? OAuthClient.environment.sandbox
-      : OAuthClient.environment.production;
-
-    // console.log(oauthClient.token);
-    // console.log(oauthClient.token.access_token);
-
-    //This is wroking. Here we are getting company info after authentication and sending it back.
-    const authResponce = await oauthClient.makeApiCall({
-      //method: 'POST',
-      // headers: {
-      //   'Content-Type': 'application/json',
-      // },
-      //body: JSON.stringify(body), 
-      // url: `${url}v3/company/${companyID}/companyinfo/${companyID}`
-      url: `${url}v3/company/${companyID}/companyinfo/${companyID}`
-    })
-
-    // This is using the quickbooks api
-    var qbo = new QuickBooks(
-      oauthClient.clientId,
-      oauthClient.clientSecret,
-      oauthClient.token.access_token,
-      false, // no token secret for oAuth 2.0
-      oauthClient.token.realmId,
-      true, // use the sandbox?
-      true, // enable debugging?
-      null, // set minorversion, or null for the latest version
-      '2.0', //oAuth version
-      oauthClient.token.refresh_token
-    );
-
-    /*
-       qbo.createAttachable({ Note: 'My Newest File' }, function (err, attachable) {
-         if (err) console.error('errrrrr', err)
-         else {
-           console.log(attachable.Id)
-           res.send(attachable)
-         }
-       })
-         */
-    /*
-        const note = qbo.findAttachables({
-          Note: 'My File'
-        }, function (e, attachables) {
-          console.log('nooooo', attachables.QueryResponse.Attachable);
-          res.send(attachables)
-        })
-    */
-    /*
-    qbo.createAccount({
-      Name: 'Yahya LFC',
-      "AccountType": "Expense"
-    }, (err, data) => {
-      if (err) console.error('errr', err)
-      else {
-        console.log(data)
-        res.send(data)
-      }
-    })
-    */
-    /*
-        qbo.findAccounts({}, (err, data) => {
-          if (err) console.error('errrrrr', err)
-          else {
-            res.send(data)
-          }
-        })
-    */
-
-    res.status(200).json(authResponce.json)
-  } catch (err) {
-    console.error(err);
-    res.status(400).json(err)
-  }
-});
 
 app.get('*', (req, res) => {
   res.status(404).send('Page not found!')
